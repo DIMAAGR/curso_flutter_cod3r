d326ef9476d96652ae87e58eb8a55b0d4d952154
DIMAAGR <attackdiamond@gmail.com>
2020-04-14 08:28:43 -0300

    Adicionado Primeira Tela

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -2,29 +2,44 @@
 
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
 
 final Category category;
-
 const CategoryItem(this.category);
+
+void _selectCategory(BuildContext context){
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (_) {
+        return CategoriesMealsScreen(category);
+      },
+    ),
+  );
+}
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(6),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(6),
+      splashColor: Theme.of(context).primaryColor,
+          child: Container(
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          ),
         ),
       ),
     );
