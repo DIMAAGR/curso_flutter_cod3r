9bd7908f56e9c01291054457c78f2ea0178813f5
DIMAAGR <attackdiamond@gmail.com>
2020-04-16 08:56:55 -0300

    Colocado Listas no Screen detail

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -5,6 +5,19 @@ import '../models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
 
+  Widget _createSectionTitle(BuildContext context, String title){
+     return Container(
+       alignment: Alignment.centerRight,
+       padding: const EdgeInsets.only(
+        right: 10 
+       ),
+        margin: EdgeInsets.symmetric(vertical: 12),
+          child: Text(
+           title,
+           style: Theme.of(context).textTheme.title,
+        ),
+    );
+  }
  
   @override
   Widget build(BuildContext context) {
@@ -41,80 +54,136 @@ class MealDetailScreen extends StatelessWidget {
           ),
         ),
         SliverFillRemaining(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 12,),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  meal.title,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.w300
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 12,),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    meal.title,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: 'RobotoCondensed',
+                      fontWeight: FontWeight.w300
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 7,),
-              Row(children: <Widget>[
-                Row(
-             mainAxisAlignment: MainAxisAlignment.center,  
-             children: <Widget>[
-               Card(
-                 elevation: 0.0,
-                 color: Colors.blueGrey.withOpacity(0.06),
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.all(Radius.circular(18)),
-                 ),
-                 child: Padding(
-                   padding: const EdgeInsets.all(2.5),
-                   child: Row(children: <Widget>[
-                    Icon(Icons.schedule),
-                    SizedBox(width: 6,),
-                    Text('${meal.duration} min'),
-                   ],),
-                 ),
-               ),
+                SizedBox(height: 7,),
+                Row(children: <Widget>[
+                  Row(
+               mainAxisAlignment: MainAxisAlignment.center,  
+               children: <Widget>[
                  Card(
-                 elevation: 0.0,
-                 color: Colors.blueGrey.withOpacity(0.06),
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.all(Radius.circular(18)),
+                   elevation: 0.0,
+                   color: Colors.blueGrey.withOpacity(0.06),
+                   shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.all(Radius.circular(18)),
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(2.5),
+                     child: Row(children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6,),
+                      Text('${meal.duration} min'),
+                     ],),
+                   ),
                  ),
-                 child: Padding(
-                   padding: const EdgeInsets.all(2.5),
-                   child: meal.costWidget,
+                   Card(
+                   elevation: 0.0,
+                   color: Colors.blueGrey.withOpacity(0.06),
+                   shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.all(Radius.circular(18)),
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(2.5),
+                     child: meal.costWidget,
+                   ),
                  ),
-               ),
-               Card(
-                 elevation: 0.0,
-                 color: Colors.blueGrey.withOpacity(0.06),
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.all(Radius.circular(23)),
+                 Card(
+                   elevation: 0.0,
+                   color: Colors.blueGrey.withOpacity(0.06),
+                   shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.all(Radius.circular(23)),
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(7),
+                     child: Row(children: <Widget>[
+                      Text('${meal.complexityText}'),
+                     ],),
+                   ),
                  ),
-                 child: Padding(
-                   padding: const EdgeInsets.all(7),
-                   child: Row(children: <Widget>[
-                    Text('${meal.complexityText}'),
-                   ],),
-                 ),
-               ),
-             ],
-           ),
-           Spacer(),
-           Padding(
-             padding: const EdgeInsets.only(
-               right: 20
+               ],
              ),
-             child: IconButton(
-               iconSize: 26,
-               icon: Icon(Icons.favorite_border),
-               onPressed: () {},
+             Spacer(),
+             Padding(
+               padding: const EdgeInsets.only(
+                 right: 20
                ),
-           ),
+               child: IconButton(
+                 iconSize: 26,
+                 icon: Icon(Icons.favorite_border),
+                 onPressed: () {},
+                 ),
+             ),
        ],),   
-                 ChipCategoryList(meal.categories),
-            ],)
+                   ChipCategoryList(meal.categories),
+                   Divider(),
+                  _createSectionTitle(context, 'Ingredientes'),
+                   Container(
+                      height: 280,
+                      child: ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: meal.ingredients.length,
+                        itemBuilder: (ctx, index){                     
+                          return Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Wrap(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                        left: 22
+                                      ),
+                                      child: Text(meal.ingredients[index],
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 20
+                                      ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Divider(),
+                              ],
+                            ),
+                          );
+                        }),  
+                    ),
+                    SizedBox(width: 20,),
+                    Container(
+                      height: 540,
+                      child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: meal.steps.length,
+                      itemBuilder: (ctx, index){
+                        return Column(
+                          children: <Widget>[
+                            ListTile(
+                            leading: CircleAvatar(
+                              child: Text('$index'),
+                              ),
+                              title: Text(meal.steps[index]),
+                      ),
+                      Divider()
+                          ],
+                        );}
+                    ),
+                  )
+              ],),
+          )
           )
       ],),
     );
